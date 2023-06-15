4fff41c40074d437dba5a278b7ce393bf39a86a3
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-15 09:55:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <unistd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -8,9 +9,7 @@
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 
-59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
 
 return(1);
 }
