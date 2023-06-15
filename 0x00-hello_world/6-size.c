1ddea40cd5fad3561fb2e61feefa3054ce138376
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-15 09:28:34 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,8 @@
-/*
- * File: 6-size.c
- * Auth: Yassine M
- */
-
 #include <stdio.h>
 
 /**
- * main - C program that prints the size of various types 
- *        base the computer it is compiled and run on..
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
@@ -18,6 +13,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
+	
 	return (0);
 }
