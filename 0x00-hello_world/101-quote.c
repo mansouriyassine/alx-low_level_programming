7c23dddfddcf7571b5b8a27f7f3fc1fdaa851f4b
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-15 09:43:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 1.
  */
-
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; write(2, message, 59);
-return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+
+return(1);
 }
