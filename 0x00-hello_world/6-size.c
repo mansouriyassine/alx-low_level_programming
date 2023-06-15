921889efb42ae75dfa2c91b163192a688ee939a8
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-15 09:13:21 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 /**
- * main - C program that prints the size of various types   
- *        on the computer it is compiled and run on.
+ * main - C program that prints the size of various types 
+ *        base the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
