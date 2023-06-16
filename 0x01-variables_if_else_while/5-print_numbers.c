a7e17677c3c771e2828c05fff9320ebfd6978bd2
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-16 11:04:52 -0700

    pritn numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+
+return (0);
+}
