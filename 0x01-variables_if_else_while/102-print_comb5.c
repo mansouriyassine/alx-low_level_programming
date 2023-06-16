743a6041aba74f546f426e46bfd58f28efba4b65
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-16 12:13:17 -0700

    ocmb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,19 @@
  */
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-int i, j;
-
-for (i = 0; i <= 99; i++)
+for (int i = 0; i <= 99; i++)
 {
-for (j = i; j <= 99; j++)
+for (int j = 0; j <= 99; j++)
 {
-int num1 = i / 10;
-int num2 = i % 10;
-int num3 = j / 10;
-int num4 = j % 10;
-
-putchar(num1 + '0');
-putchar(num2 + '0');
+if (i != j)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(num3 + '0');
-putchar(num4 + '0');
-
-if (i != 99 || j != 99)
-{
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 putchar(',');
 putchar(' ');
 }
