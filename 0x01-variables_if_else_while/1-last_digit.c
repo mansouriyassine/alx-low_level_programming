98b3b9cbdff2d30f8e03d7c338eba234f1348c1d
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-16 09:32:13 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %d\n and is less than 6 and not 0", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 
 return (0);
