5a06a46bdaabdb776fd15a9fe20986f348b39c50
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-16 09:41:17 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** 
+ * main - prints the alphabet
+ *
+ * Return : always 0
+ *
+ */
+int main(void)
+{
+char  alpha = 'a'
+
+while (alpha <= 'z')
+{
+putchar (alpha);
+alpha++;
+}
+putchar ('\n');
+
+return (0);
+}
