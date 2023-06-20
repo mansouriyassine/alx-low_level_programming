ea4ddd98b087f36baab95e5267117b1af63112d4
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 14:59:43 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,15 @@
 void jack_bauer(void)
 {
 int h, m;
-for (h == 00, h <= 24, h++)
+for (h = 0, h <= 23, h++)
 {
-for (m == 00; m <= 59, m++)
+for (m = 0; m <= 59, m++)
 {
-_putchar(h);
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar(m);
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
