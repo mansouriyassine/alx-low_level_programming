c4279f89086cd1a8c6ca0c1bf13b529a4749db45
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 16:37:07 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
-for (i = n ; i <= 98 ; i++)
+for (i = n ; i <= 98; i++)
 {
 _putchar(i + '0');
 _putchar(',');
@@ -26,7 +26,7 @@ _putchar('8');
 }
 else
 {
-for (i = n ; i >= 98 ; i--);
+for (i = n ; i >= 98; i--)
 {
 _putchar(i + '0');
 _putchar(',');
