8851b3f832457dc0e0e720b943691be577c18923
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 16:09:51 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ *
+ * @n: starting number
+ *
+ * Return : nothing
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n ; i<= 98; i++)
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+else if (n == 98)
+{
+_putchar(98)
+_putchar(98)
+}
+else
+{
+for (i =n ;i >= 98; i--);
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+}
