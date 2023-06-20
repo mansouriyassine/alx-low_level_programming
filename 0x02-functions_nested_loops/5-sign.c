2bfb578d3294746fb76c4145f4488629c5a282f5
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 12:42:30 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
+printf("+1");
 return (1);
 }
 else if (n == 0)
 {
-printf("0");
+printf("00");
 return (0);
 }
 else
 {
-printf("-");
+printf("-1");
 return (-1);
 }
 }
