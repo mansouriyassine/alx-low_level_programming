3a8f6c3eb09b085be065892c7bc837c946e00573
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 14:29:33 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void);
+{
+int h, m;
+for (h == 00, h <= 24, h++)
+{
+for (m == 00; m <= 59, m++)
+{
+_putchar(h);
+_putchar(':');
+_putchar(m);
+_putchar('\n');
+}
+}
