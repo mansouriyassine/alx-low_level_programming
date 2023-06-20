e03c6ecdd8d60256701383e595fd8ac1bde7e9d2
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 11:36:57 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - return 1 if lowwercase and 0 otherwise
+ *
+ * return : nothing 
+ */
+int _islower(int c);
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
