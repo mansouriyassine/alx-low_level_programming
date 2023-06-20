102b83b8e574cd0d53d5b7fffa3b4f15fb4a3af0
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 15:37:55 -0700

    9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,10 @@
 void times_table(void)
 {
 int i, j;
-for (i = 0; i<= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j<= 9; j++)
-{       
+for (j = 0; j <= 9; j++)
+{
 int A = i * j;
 if (A <= 9)
 {
