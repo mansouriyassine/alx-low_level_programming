b271c3d38e0f4fc151ef0fede9fba9e693f7357a
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 10:07:29 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet
+ *
+ * Return : nothing
+ */
+void print_alphabet(void);
+{
+char LETTER = 'a'
+
+while (LETTER <= 'z')
+{
+_putchar ('LETTER');
+LETTER++;
+}
+_putchar('\n');
+}
