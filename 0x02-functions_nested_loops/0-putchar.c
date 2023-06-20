b51cca24860162b5c32b809a48cd77b220d77c38
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 09:39:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+/**
+  * main - Print _putchar
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-printf ("_putchar\n");
-}
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');	
 
 return (0);
+}
