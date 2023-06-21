bf93912c64b802159b3879e06ee27bd43e301646
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 18:30:14 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,46 +7,46 @@
 
 void print_times_table(int n)
 {
-    int A, B, C;
+	int A, B, C;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (A = 0; A <= n; A++)
-        {
-            for (B = 0; B <= n; B++)
-            {
-                C = B * A;
+	if (n >= 0 && n <= 15)
+	{
+		for (A = 0; A <= n; A++)
+		{
+			for (B = 0; B <= n; B++)
+			{
+				C = B * A;
 
-                if (B == 0)
-                {
-                    _putchar(C + '0');
-                }
-                else if (C < 10 && B != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(C + '0');
-                }
-                else if (C >= 10 && C < 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar((C / 10) + '0');
-                    _putchar((C % 10) + '0');
-                }
-                else if (C >= 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar((C / 100) + '0');
-                    _putchar(((C / 10) % 10) + '0');
-                    _putchar((C % 10) + '0');
-                }
-            }
-            _putchar('\n');
-        }
-    }
+				if (B == 0)
+				{
+					_putchar(C + '0');
+				}
+				else if (C < 10 && B != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(C + '0');
+				}
+				else if (C >= 10 && C < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((C / 10) + '0');
+					_putchar((C % 10) + '0');
+				}
+				else if (C >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((C / 100) + '0');
+					_putchar(((C / 10) % 10) + '0');
+					_putchar((C % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
