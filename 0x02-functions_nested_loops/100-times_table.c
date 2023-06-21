166fce9c53de9fcc84383e3cb677903294ff114d
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 18:28:17 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * print_times_table - prints 10 times tables
- *
- * @n: the number in the input
- *
- * Return: nothing
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
-int A, B, C;
+    int A, B, C;
 
-if (0 <= n && n <= 14)
-{
-for (A = 0; A <= n; A++)
-{
-for (B = 0; B <= n; B++)
-{
-C = A * B;
-if (99 < C)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((C / 100) + '0');
-_putchar(((C / 10) % 10) + '0');
-_putchar((C % 10) + '0');
-}
-else if (9 < C)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(((C / 10) % 10) + '0');
-_putchar((C % 10) + '0');
-}
-else
-{
-if (B != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-_putchar(C + '0');
-}
-}
-_putchar('\n');
-}
-}
+    if (n >= 0 && n <= 15)
+    {
+        for (A = 0; A <= n; A++)
+        {
+            for (B = 0; B <= n; B++)
+            {
+                C = B * A;
+
+                if (B == 0)
+                {
+                    _putchar(C + '0');
+                }
+                else if (C < 10 && B != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(C + '0');
+                }
+                else if (C >= 10 && C < 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar((C / 10) + '0');
+                    _putchar((C % 10) + '0');
+                }
+                else if (C >= 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((C / 100) + '0');
+                    _putchar(((C / 10) % 10) + '0');
+                    _putchar((C % 10) + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
