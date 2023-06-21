93078a84dd6c8f11af55f9e9eb95adebba2723ea
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 17:59:12 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@
 void print_times_table(int n)
 {
 int A, B, C;
-if (0 <= n && 14 >= n)
+if (0 <= 0 && 14 >= n)
 {
 for (A = 0; A <= n; A++)
 {
@@ -18,7 +18,7 @@ for (B = 0; B <= n; B++)
 {
 C = A * B;
 if (C > 99)
-
+{
 _putchar(',');
 _putchar(32);
 _putchar((C / 100) + '0');
