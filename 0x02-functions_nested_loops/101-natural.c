745a5750e5cf308bb1b6db0f302780dfc9b68ed3
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 18:39:06 -0700

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-int main() {
-int L = 1024;
-int S = 0;
-int i;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int L = 1024;
+    int S = 0;
+    int i;
 
-for (i = 1; i < L; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
-S += i;
-}
+    for (i = 1; i < L; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            S += i;
+    }
+
+    printf("%d\n", S);
+
+    return 0;
 }
 
-printf("%d\n", S);
-
-return (0);
-}
