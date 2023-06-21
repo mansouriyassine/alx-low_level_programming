7a9ab05d11a8962bbf0b9ae45aa95d6768ea5cef
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-21 08:28:23 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* main - Fonction that rest for negative or positive
-* 
-*  Return: zero
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-int a;
-a = 0;
-positive_or_negative(a);
+int i;
+
+i = 0;
+positive_or_negative(i);
 
 return (0);
 }
