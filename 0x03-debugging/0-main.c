0c4d9846414f2841e4943f3032b943e3071e58bd
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-21 08:18:59 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - Fonction that rest for negative or positive
+* 
+*  Return: zero
+*/
+int main(void)
+{
+int a;
+a = 0;
+positive_or_negative(a);
+
+return (0);
+}
