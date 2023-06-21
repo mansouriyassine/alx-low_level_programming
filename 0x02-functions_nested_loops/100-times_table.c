28ceedcc493b4e1223f1713f818b8afcd6e83068
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 17:18:44 -0700

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints 10 times tables
+ *
+ *@n: the number in the input
+ *
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+int A, B, C;
+if (n >= 0 && n <= 14)
+{
+for (A = 0; A <= n; A++)
+{
+for (B = 0; B <= n; B++)
+{
+C = A * B;
+if (C > 99)
+{
+_putchar(',');
+_putchar(32);
+_putchar((C / 100) + '0');
+_putchar(((C / 10) % 10) + '0');
+_putchar((C % 10) + '0');
+}
+else if (C > 9)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(((C / 10) % 10) + '0');
+_putchar((C % 10) + '0');
+}
+else
+{
+if (B != 0)
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(C + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
