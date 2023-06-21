09b4ebb751d0b109649a8f81ce4b8eb12d7eea89
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 18:34:22 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int L = 1024;
+int S = 0;
+
+for (int i = 1; i < L; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+S += i;
+}
+}
+
+printf("%d\n", S);
+
+return (0);
+}
