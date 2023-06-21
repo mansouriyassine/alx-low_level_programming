7ae6c12746d3247717d1612f2da29362e2369a7f
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-20 17:08:33 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,36 @@
  */
 void print_to_98(int n)
 {
-int i;
-if (n < 98)
+if (n <= 98)
 {
-for (i = n ; i <= 98; i++)
+for (; n <= 98; n++)
 {
-printf("%d%c", i, (i == 98) ? '\n' : ',');
-}
-}
-else if (n == 98)
+if (n == 98)
 {
-printf("%d%c", i, (i == 98) ? '\n' : ',');
+printf("%d", n);
+printf("\n");
+break;
 }
 else
 {
-for (i = n ; i >= 98; i--)
+printf("%d, ", n);
+}
+}
+}
+else
 {
-printf("%d , ", i);
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
 }
 }
 }
