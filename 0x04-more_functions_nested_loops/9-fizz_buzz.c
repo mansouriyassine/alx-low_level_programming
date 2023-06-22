090d19fa6235bc14a27ab30cb44a5a34482ea07a
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-22 08:36:10 -0700

    FB

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - program that prints fizz or buzz or fizzBuzz
+*
+*Return: returns 0
+*/
+int main()
+{
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+
+printf("\n");
+return 0;
+}
