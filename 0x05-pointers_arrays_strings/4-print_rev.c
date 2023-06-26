146c879d12038756580512b423049453a5f01735
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-26 16:37:22 -0700

    prints rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+int length = 0;
+while (*(s + length) != '\0')
+{
+length++;
+}
+
+for (int i = length - 1; i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+
+_putchar('\n');
+}
