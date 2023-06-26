a2d4f52c0ca84667f602467a97dd390140aba71d
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-26 16:22:01 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * _strlen - the length of a string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+return (length);
+}
