e595c6cfece544d125374f047685d882af2a4018
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-26 17:03:38 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+putchar('\n');
+}
