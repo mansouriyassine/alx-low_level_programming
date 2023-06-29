952af6dc9c0e33f4d96aee9be3ac6d8642edd6b8
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-29 12:00:59 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+int divisor = 1;
+
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+
+while (n / divisor > 9)
+{
+divisor *= 10;
+}
+
+while (divisor != 0)
+{
+_putchar('0' + (n / divisor));
+n %= divisor;
+divisor /= 10;
+}
+}
+
