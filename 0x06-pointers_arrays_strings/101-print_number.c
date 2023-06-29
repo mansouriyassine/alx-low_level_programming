d193f65e157639efc98fa229f0f959bef9f90b3f
mansouriyassine <mansouri.yassine1@gmail.com>
2023-06-29 16:26:24 -0700

    print n

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,23 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_number - prints an integer
- * @n: parameter
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_number - print numbers chars
+ * @n: integer params
  * Return: 0
  */
-
 void print_number(int n)
 {
 unsigned int i;
