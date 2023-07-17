e7915fc094379484d15dd19ad826186ab7299692
mansouriyassine <mansouri.yassine1@gmail.com>
2023-07-17 07:28:33 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,12 +4,15 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog structure and returns a pointer to it.
+ * new_dog - Creates a new dog structure and returns
+ * a pointer to it.
+ *
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
- * Return: (Pointer to the newly created dog structure), or (NULL) on failure.
+ * Return: (Pointer to the newly created dog structure),
+ * or (NULL) on failure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
