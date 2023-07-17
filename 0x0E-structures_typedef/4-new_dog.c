b7195c3c5315b42dd971b01666ca253e12c33931
mansouriyassine <mansouri.yassine1@gmail.com>
2023-07-17 07:38:24 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,33 +9,38 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
- * Return: (Pointer to the newly created dog structure), or (NULL) on failure.
+ * Return: Pointer to the newly created dog structure, or NULL on failure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 if (name == NULL || owner == NULL)
 return (NULL);
 
-dog_t *new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+dog_t *cutie_dog = malloc(sizeof(dog_t));
+if (cutie_dog == NULL)
 return (NULL);
 
-new_dog->name = strdup(name);
-if (new_dog->name == NULL)
+int name_len = strlen(name);
+int owner_len = strlen(owner);
+
+cutie_dog->name = malloc(name_len + 1);
+if (cutie_dog->name == NULL)
 {
-free(new_dog);
+free(cutie_dog);
 return (NULL);
 }
 
-new_dog->owner = strdup(owner);
-if (new_dog->owner == NULL)
+cutie_dog->owner = malloc(owner_len + 1);
+if (cutie_dog->owner == NULL)
 {
-free(new_dog->name);
-free(new_dog);
+free(cutie_dog->name);
+free(cutie_dog);
 return (NULL);
 }
 
-new_dog->age = age;
+strcpy(cutie_dog->name, name);
+strcpy(cutie_dog->owner, owner);
+cutie_dog->age = age;
 
-return (new_dog);
+return (cutie_dog);
 }
