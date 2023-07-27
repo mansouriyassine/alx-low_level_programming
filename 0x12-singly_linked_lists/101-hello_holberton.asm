dd5c3fe1830fa974f2c61ede37a17718d8fe0e55
mansouriyassine <mansouri.yassine1@gmail.com>
2023-07-27 08:42:21 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,21 @@
-section .data
-hello db "Hello, Holberton", 0
-format db "%s", 10, 0   ; "%s" is the format specifier for a string, 10 is ASCII for newline, 0 is the null terminator
-
-section .text
 extern printf
 
+section .text
 global main
+
 main:
-; Call printf function to print the message
-mov rdi, format   ; Format specifier ("%s\n")
-mov rsi, hello    ; Address of the string to be printed
-xor rax, rax      ; Clear RAX register (required by printf calling convention)
+push rbp
+
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
 call printf
 
-; Exit the program
-mov rax, 60       ; syscall number for sys_exit
-xor rdi, rdi      ; exit status 0
-syscall
+pop rbp
+
+mov rax, 0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
