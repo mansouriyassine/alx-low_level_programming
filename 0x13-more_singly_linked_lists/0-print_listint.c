672b0a6e8757c71af3b98541b1b074567df9c18c
mansouriyassine <mansouri.yassine1@gmail.com>
2023-07-31 13:22:20 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
+const listint_t *current = h;
 size_t count = 0;
 
-while (h != NULL)
+while (current != NULL)
 {
-printf("%d\n", h->n);
-h = h->next;
+printf("%d\n", current->n);
 count++;
+current = current->next;
 }
 
 return (count);
