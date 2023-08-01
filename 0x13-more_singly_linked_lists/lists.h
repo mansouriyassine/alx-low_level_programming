2ce14065d53b6a0cda6ac5c15a3c27beafdb98e6
mansouriyassine <mansouri.yassine1@gmail.com>
2023-07-31 17:15:03 -0700

    get node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Structure for the singly linked list of integers */
+#include <stddef.h>
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
