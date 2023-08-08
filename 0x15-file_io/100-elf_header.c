ecf9c21c2d4b1b22283e9c8a4f500df49e2e25c7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-08-07 19:58:56 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,36 +6,42 @@
 
 #define ELF_MAGIC 0x7f454c46
 
-int main(int argc, char *argv[]) {
-  int fd;
-  unsigned char magic[4];
+int main(int argc, char *argv[])
+{
+int fd;
+unsigned char magic[4];
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: elf_header elf_filename\n");
-    exit(1);
-  }
+if (argc != 2)
+{
+fprintf(stderr, "Usage: elf_header elf_filename\n");
+exit(1);
+}
 
-  fd = open(argv[1], O_RDONLY);
-  if (fd == -1) {
-    perror("open");
-    exit(1);
-  }
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
+{
+perror("open");
+exit(1);
+}
 
-  if (read(fd, magic, sizeof(magic)) != sizeof(magic)) {
-    perror("read");
-    exit(1);
-  }
+if (read(fd, magic, sizeof(magic)) != sizeof(magic))
+{
+perror("read");
+exit(1);
+}
 
-  printf("ELF Header:\n");
-  printf("  Magic:   %02x %02x %02x %02x\n", magic[0], magic[1], magic[2], magic[3]);
-  printf("  Class:                             %d\n", magic[4]);
-  printf("  Data:                             %d\n", magic[5]);
-  printf("  Version:                           %d\n", magic[6]);
-  printf("  OS/ABI:                           %d\n", magic[7]);
-  printf("  ABI Version:                       %d\n", magic[8]);
-  printf("  Type:                             %d\n", magic[9]);
-  printf("  Entry point address:               %#x\n", *(unsigned int *)(magic + 10));
+printf("ELF Header:\n");
+printf("  Magic:   %02x %02x %02x %02x\n",
+		magic[0], magic[1], magic[2], magic[3]);
+printf("  Class:                             %d\n", magic[4]);
+printf("  Data:                             %d\n", magic[5]);
+printf("  Version:                           %d\n", magic[6]);
+printf("  OS/ABI:                           %d\n", magic[7]);
+printf("  ABI Version:                       %d\n", magic[8]);
+printf("  Type:                             %d\n", magic[9]);
+printf("  Entry point address:               %#x\n",
+		*(unsigned int *)(magic + 10));
 
-  close(fd);
-  return 0;
+close(fd);
+return (0);
 }
