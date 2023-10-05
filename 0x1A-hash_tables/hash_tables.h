b65de08b6a89ccf0db9bd98890bc4637b992df3a
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-05 13:59:25 -0700

    header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,7 +38,12 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *create_pair(const char *key, const char *value);
-int set_pair_only(hash_table_t *ht, const char *key, const char *value, unsigned long int size);
+int set_pair_only(
+hash_table_t *ht,
+const char *key,
+const char *value,
+unsigned long int size
+);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
@@ -83,9 +88,19 @@ shash_node_t *stail;
 
 shash_table_t *shash_table_create(unsigned long int size);
 shash_node_t *create_spair(const char *key, const char *value);
-shash_node_t *set_spair_only(shash_table_t *ht, const char *key, const char *value, unsigned long int index);
+shash_node_t *set_spair_only(
+shash_table_t *ht,
+const char *key,
+const char *value,
+unsigned long int index
+);
 int update_value(shash_node_t *node, const char *value);
-shash_node_t *set_spair_front(shash_table_t *ht, const char *key, const char *value, unsigned long int index);
+shash_node_t *set_spair_front(
+shash_table_t *ht,
+const char *key,
+const char *value,
+unsigned long int index
+);
 int slist_set_first(shash_table_t *ht, shash_node_t *node);
 int slist_set(shash_table_t *ht, shash_node_t *old_node, shash_node_t *node);
 int slist_set_end(shash_table_t *ht, shash_node_t *node);
