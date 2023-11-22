73312c084a3399948a50dc4a85c5739602d12806
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-22 13:01:45 -0800

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,32 +2,21 @@
 
 def island_perimeter(grid):
     """
-    Compute the perimeter of an island represented in a grid.
-
-    Args:
-    grid (list of lists of int): A grid where
-    '1' represents land and '0' represents water.
-
-    Returns:
-    int: The total perimeter of the island.
+    Returns the perimeter of the island.
+    Grid represents water by 0 and land by 1.
     """
-    if not grid:
-        return 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    row_count, col_count = len(grid), len(grid[0])
-    perimeter = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
 
-    for row in range(row_count):
-        for col in range(col_count):
-            if grid[row][col] == 1:
-                perimeter += 4
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1
-                if row < row_count - 1 and grid[row + 1][col] == 1:
-                    perimeter -= 1
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1
-                if col < col_count - 1 and grid[row][col + 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
+    return size * 4 - edges * 2
