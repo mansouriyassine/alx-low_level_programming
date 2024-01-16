015d036e9688a166b4e74a18a12f63b8d51ac682
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-16 05:13:17 -0800

    100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -6,7 +6,7 @@
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
- * Return: First index where value is located, or -1 if not found or array is NULL
+ * Return: First index where value is located, or -1 if not found
  */
 int jump_search(int *array, size_t size, int value)
 {
@@ -22,12 +22,12 @@ while (step < size && array[step] < value)
 printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 prev = step;
 step += jump;
-if (step > size - 1)
-step = size;
 }
 
-printf("Value found between indexes [%lu] and [%lu]\n", 
-prev, step < size ? step : size - 1);
+if (step >= size)
+step = size - 1;
+
+printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
 
 for (i = prev; i <= step && i < size; i++)
 {
