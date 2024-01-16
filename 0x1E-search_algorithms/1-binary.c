2047c2f70e0d4640e048497e0ba23d5c1c714ef2
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-16 04:52:24 -0800

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,26 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @left: Left index of the subarray
+ * @right: Right index of the subarray
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+size_t i;
+
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i < right)
+printf(", ");
+}
+printf("\n");
+}
+
+/**
  * binary_search - Searches for a value in a sorted array of integers
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
@@ -10,15 +30,20 @@
 int binary_search(int *array, size_t size, int value)
 {
 size_t mid, left = 0, right = size - 1;
+
 while (array && left <= right)
 {
+print_array(array, left, right);
 mid = left + (right - left) / 2;
+
 if (array[mid] == value)
 return ((int)mid);
+
 if (array[mid] < value)
 left = mid + 1;
 else
 right = mid - 1;
 }
+
 return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,6 +13,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
 
 /* Data structures */
 
