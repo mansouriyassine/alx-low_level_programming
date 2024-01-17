82d7636bc2d79c34e30d01c0495e9279ea9054c4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-17 14:51:10 -0800

    105

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for a value in a sorted list using the jump search
+ * jump_list - Searches for a value in a sorted list using jump search
  * @list: Pointer to the head of the list to search in
  * @size: The number of nodes in the list
  * @value: The value to search for
@@ -12,6 +12,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 size_t jump_step = sqrt(size);
 size_t index, i;
 listint_t *prev, *current;
+char message[100];
 
 if (list == NULL || size == 0)
 return (NULL);
@@ -23,16 +24,24 @@ prev = current;
 for (i = 0; i < jump_step && current->next; i++)
 current = current->next;
 
-printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+printf("Value checked at index [%lu] = [%d]\n",
+current->index, current->n);
 if (current->n >= value)
 break;
 }
 
-printf("Value found between indexes [%lu] and [%lu]\n", prev->index, current->index);
+snprintf(message, sizeof(message),
+"Value found between indexes [%lu] and [%lu]\n",
+prev->index, current->index);
 
-for (index = prev->index; index <= current->index && prev->next; index++)
+printf("%s", message);
+
+for (index = prev->index;
+index <= current->index && prev->next;
+index++)
 {
-printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+printf("Value checked at index [%lu] = [%d]\n",
+prev->index, prev->n);
 if (prev->n == value)
 return (prev);
 prev = prev->next;
